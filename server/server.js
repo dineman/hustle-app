c5c342e85e8d78d76d0d9cb41ea7b5fc4e201d1f
focus <focus@pop-os.localdomain>
2023-07-01 20:36:40 +0530

    sign up more improved

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 import express from 'express'
-import mongoose from 'mongoose'
-const app = express()
+import mongoose from 'mongoose' 
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dontenv from 'dotenv'
+import multer from 'multer'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-app.get("/api",(req,res)=>{
-    res.json({"users":["userOne","userTwo","userThree"]})
-})
+
+const app = express()
+app.use(bodyParser.json());
+
+
+app.post('/api', async (req, res) => {
+    try {
+      const { name, phone, password } = req.body;
+
+
+  
+      res.status(200).json({ message: 'Data received successfully' });
+      console.log(req.body)
+    } catch (error) {
+      console.error('Error receiving data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 app.listen(4000,()=>{
     console.log("listining at post 4000")
